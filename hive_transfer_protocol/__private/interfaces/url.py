a905b4995e9c41980a0de268952c3eb0d99b1911
kmochocki <kmochocki@syncad.com>
2023-08-12 18:47:16 +0000

    Move and adjust url class from test-tools

---
+++ hive_transfer_protocol/__private/interfaces/url.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from typing import Generic, Literal, TypeVar
+from urllib.parse import urlparse
+
+HttpProtocolT = Literal["http", "https"]
+WsProtocolT = Literal["ws", "wss"]
+ProtocolT = TypeVar("ProtocolT", bound=HttpProtocolT | WsProtocolT)
+
+
+class Url(Generic[ProtocolT]):
+    """Wrapper for Url, for handy access to all of it members with serialization."""
+
+    def __init__(self, url: str, *, protocol: ProtocolT | Literal[""] = "") -> None:
+        parsed_url = urlparse(url, scheme=protocol)
+        if not parsed_url.hostname:
+            raise ValueError("Address was not specified.")
+
+        self.__protocol = protocol
+        self.__address: str = parsed_url.hostname
+        self.__port: int | None = parsed_url.port
+
+    @property
+    def protocol(self) -> ProtocolT | Literal[""]:
+        """Return protocol of url, e.x: http, https."""
+        return self.__protocol
+
+    @property
+    def address(self) -> str:
+        """Return address or hostname of url, e.x: 127.0.0.1, localhost."""
+        return self.__address
+
+    @property
+    def port(self) -> int | None:
+        """Return port of url, e.x: 0, 8090."""
+        return self.__port
+
+    def as_string(self, *, with_protocol: bool = True) -> str:
+        """Serializes url."""
+        protocol_prefix = f"{self.protocol}://" if with_protocol else ""
+        port_suffix = f":{self.port}" if self.port else ""
+
+        return f"{protocol_prefix}{self.address}{port_suffix}"
+
+
+HttpUrl = Url[HttpProtocolT]
+WsUrl = Url[WsProtocolT]
+AnyUrl = Url[HttpProtocolT] | Url[WsProtocolT]
