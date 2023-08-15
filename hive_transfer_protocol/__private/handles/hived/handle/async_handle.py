5ea5a60d4da8b09833ef147ef29d74fbaad2e3a3
kmochocki <kmochocki@syncad.com>
2023-08-15 19:04:22 +0000

    Add Time based methods to hived handles

--- hive_transfer_protocol/__private/handles/hived/handle/async_handle.py
+++ hive_transfer_protocol/__private/handles/hived/handle/async_handle.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import math
 from typing import TYPE_CHECKING, cast
 
 from hive_transfer_protocol.__private.handles.abc.handle import AbstractAsyncHandle
@@ -7,9 +8,11 @@ from hive_transfer_protocol.__private.handles.hived.api.api_collection import (
     HivedAsyncApiCollection,
 )
 from hive_transfer_protocol.__private.handles.hived.handle.common_helpers import HiveHandleCommonHelpers
+from hive_transfer_protocol.__private.interfaces.time import Time
+from hive_transfer_protocol.exceptions import BlockWaitTimeoutError
 
 if TYPE_CHECKING:
-    from datetime import datetime
+    from datetime import datetime, timedelta
 
     from schemas.__private.hive_fields_basic_schemas import AccountName
 
@@ -39,3 +42,51 @@ class AsyncHived(AbstractAsyncHandle, HiveHandleCommonHelpers):
 
     async def get_current_witness(self) -> AccountName:
         return self._get_current_witness(await self.get_dynamic_global_properties())
+
+    async def wait_for_block_with_number(self, block_number: int, *, timeout: float | timedelta = math.inf) -> None:
+        async def __is_block_with_number_reached() -> bool:
+            last = await self.get_last_block_number()
+            return last >= block_number
+
+        await Time.async_wait_for(
+            __is_block_with_number_reached,
+            timeout=timeout,
+            timeout_error_message=f"Waiting too long for block {block_number}",
+            poll_time=2.0,
+        )
+
+    async def wait_for_irreversible_block(
+        self, number: int | None = None, *, max_blocks_to_wait: int | None = None, timeout: float | timedelta = math.inf
+    ) -> None:
+        last_block_number = await self.get_last_block_number()
+        target_block_number = number or last_block_number
+        max_wait_block_number = (last_block_number + max_blocks_to_wait) if max_blocks_to_wait is not None else None
+
+        async def __is_block_with_number_irreversible() -> bool:
+            if max_wait_block_number is not None:
+                await self.__assert_block_with_number_reached_irreversibility_before_limit(
+                    target_block_number, max_wait_block_number
+                )
+
+            last_irreversible_block_number = await self.get_last_irreversible_block_number()
+            return last_irreversible_block_number >= target_block_number
+
+        await Time.async_wait_for(
+            __is_block_with_number_irreversible,
+            timeout=timeout,
+            timeout_error_message=f"Waiting too long for irreversible block {target_block_number}",
+            poll_time=2.0,
+        )
+
+    async def __assert_block_with_number_reached_irreversibility_before_limit(
+        self, block_number: int, max_wait_block_number: int
+    ) -> None:
+        response = await self.get_dynamic_global_properties()
+        last_block_number = response.head_block_number
+        last_irreversible_block_number = response.last_irreversible_block_num
+
+        def __expected_block_was_reached_but_is_still_not_irreversible() -> bool:
+            return last_block_number >= max_wait_block_number and last_irreversible_block_number < block_number
+
+        if __expected_block_was_reached_but_is_still_not_irreversible():
+            raise BlockWaitTimeoutError(last_block_number, block_number, last_irreversible_block_number)
--- hive_transfer_protocol/exceptions.py
+++ hive_transfer_protocol/exceptions.py
@@ -7,3 +7,21 @@ class HiveTransferProtocolError(Exception):
 
 class ParseError(HiveTransferProtocolError):
     """Raised if cannot parse given str, e.x. url, date, asset."""
+
+
+class BlockWaitTimeoutError(HiveTransferProtocolError):
+    """Raised if reached not expected block number."""
+
+    def __init__(self, last_block_number: int, block_number: int, last_irreversible_block_number: int) -> None:
+        """Creates exception.
+
+        Arguments:
+            last_block_number -- last fetched block number
+            block_number -- block that was expected to be irreversible
+            last_irreversible_block_number -- last fetched irreversible block number
+        """
+        super().__init__(
+            f"Block with number `{last_block_number}` was just reached but expected `{block_number}` is still not"
+            " irreversible.\n"
+            f"Last irreversible block number is `{last_irreversible_block_number}`."
+        )
