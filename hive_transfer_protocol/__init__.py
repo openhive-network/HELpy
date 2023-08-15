53040a3277cc1f1ed795b859faadaab10ac2a443
kmochocki <kmochocki@syncad.com>
2023-08-15 00:08:11 +0000

    Add hived and wax to public interface

--- hive_transfer_protocol/__init__.py
+++ hive_transfer_protocol/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
 from hive_transfer_protocol.__private.handles.beekeeper.handle import AsyncBeekeeper, Beekeeper
+from hive_transfer_protocol.__private.handles.hived.handle import AsyncHived, Hived
+from hive_transfer_protocol.__private.interfaces import wax
 
-__all__ = ["Beekeeper", "AsyncBeekeeper"]
+__all__ = ["Beekeeper", "AsyncBeekeeper", "AsyncHived", "Hived", "wax"]
