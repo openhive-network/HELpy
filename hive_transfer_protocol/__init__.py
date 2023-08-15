67619bbf482fd7710123938af0ce9395b1d95ebc
kmochocki <kmochocki@syncad.com>
2023-08-15 18:40:55 +0000

    Add Time utility class

--- hive_transfer_protocol/__init__.py
+++ hive_transfer_protocol/__init__.py
@@ -3,5 +3,6 @@ from __future__ import annotations
 from hive_transfer_protocol.__private.handles.beekeeper.handle import AsyncBeekeeper, Beekeeper
 from hive_transfer_protocol.__private.handles.hived.handle import AsyncHived, Hived
 from hive_transfer_protocol.__private.interfaces import wax
+from hive_transfer_protocol.__private.interfaces.time import Time, TimeFormats
 
-__all__ = ["Beekeeper", "AsyncBeekeeper", "AsyncHived", "Hived", "wax"]
+__all__ = ["Beekeeper", "AsyncBeekeeper", "AsyncHived", "Hived", "Time", "TimeFormats", "wax"]
--- hive_transfer_protocol/exceptions.py
+++ hive_transfer_protocol/exceptions.py
@@ -3,3 +3,7 @@ from __future__ import annotations
 
 class HiveTransferProtocolError(Exception):
     """Base class for all Hive Transfer Protocol Errors."""
+
+
+class ParseError(HiveTransferProtocolError):
+    """Raised if cannot parse given str, e.x. url, date, asset."""
