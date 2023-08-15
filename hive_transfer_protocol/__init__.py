223c68ee9b5a2930e114f5c849c0d76f4e31543b
kmochocki <kmochocki@syncad.com>
2023-08-15 19:53:39 +0000

    Expose Url class to library interface

--- hive_transfer_protocol/__init__.py
+++ hive_transfer_protocol/__init__.py
@@ -4,5 +4,6 @@ from hive_transfer_protocol.__private.handles.beekeeper.handle import AsyncBeeke
 from hive_transfer_protocol.__private.handles.hived.handle import AsyncHived, Hived
 from hive_transfer_protocol.__private.interfaces import wax
 from hive_transfer_protocol.__private.interfaces.time import Time, TimeFormats
+from hive_transfer_protocol.__private.interfaces.url import HttpUrl, WsUrl
 
-__all__ = ["Beekeeper", "AsyncBeekeeper", "AsyncHived", "Hived", "Time", "TimeFormats", "wax"]
+__all__ = ["Beekeeper", "AsyncBeekeeper", "AsyncHived", "Hived", "HttpUrl", "WsUrl", "Time", "TimeFormats", "wax"]
