b767d0f2b5b833200c7ba1a5be0d83420201fd5d
kmochocki <kmochocki@syncad.com>
2023-10-01 03:04:34 +0000

    Move wait_number_of_blocks method from tt

--- helpy/_handles/hived/async_handle.py
+++ helpy/_handles/hived/async_handle.py
@@ -43,6 +43,10 @@ class AsyncHived(AbstractAsyncHandle, HiveHandleCommonHelpers):
     async def get_current_witness(self) -> AccountName:
         return self._get_current_witness(await self.get_dynamic_global_properties())
 
+    async def wait_number_of_blocks(self, blocks_to_wait: int, *, timeout: float = math.inf) -> None:
+        assert blocks_to_wait > 0
+        await self.wait_for_block_with_number(await self.get_last_block_number() + blocks_to_wait, timeout=timeout)
+
     async def wait_for_block_with_number(self, block_number: int, *, timeout: float | timedelta = math.inf) -> None:
         async def __is_block_with_number_reached() -> bool:
             last = await self.get_last_block_number()
