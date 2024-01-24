233286bc195a786d603912dc5685f46188bee532
kmochocki <kmochocki@syncad.com>
2024-01-24 17:43:14 +0000

    Pre-commit cleanup

--- helpy/_handles/abc/build_json_rpc_call.py
+++ helpy/_handles/abc/build_json_rpc_call.py
@@ -4,7 +4,9 @@ from __future__ import annotations
 def build_json_rpc_call(*, method: str, params: str, id_: int = 0) -> str:
     """Builds params for jsonrpc call."""
     return (
-        """{"id":""" + str(id_) + ""","jsonrpc":"2.0","method":\""""
+        """{"id":"""
+        + str(id_)
+        + ""","jsonrpc":"2.0","method":\""""
         + method
         + '"'
         + (""","params":""" + params if params else "")
