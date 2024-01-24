dc08f7afaf69a25c78a318be8abc45f5010bbbb3
kmochocki <kmochocki@syncad.com>
2024-01-24 17:25:39 +0000

    Move CommunicationError

--- helpy/exceptions.py
+++ helpy/exceptions.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from typing import Any
+
+JsonT = dict[str, Any]
+CommunicationResponseT = str | JsonT | list[JsonT]
 
 
 class HelpyError(Exception):
@@ -35,3 +39,84 @@ class RequestError(HelpyError):
 
     send: str
     error: str
+
+
+class BatchRequestError(HelpyError):
+    """Base class for batch related errors."""
+
+
+class NothingToSendError(BatchRequestError):
+    """Raised if there is nothing to send, example.
+
+    with node.batch():
+        pass
+        # here on exit NothingToSendError will be raised
+    """
+
+
+class ResponseNotReadyError(BatchRequestError):
+    """Raised on access to response when it's not ready.
+
+    with node.batch() as x:
+        resp = x.api.some_endpoint()
+        assert resp.some_var == 1  # here ResponseNotReadyError will be raised
+    """
+
+
+class CommunicationError(HelpyError):
+    """Base class for all communication related errors."""
+
+    def __init__(
+        self, url: str, request: str, response: CommunicationResponseT | None = None, *, message: str = ""
+    ) -> None:
+        """Contains required details.
+
+        Args:
+            url (str): where request has been send
+            request (str): content of request
+            response (CommunicationResponseT | None, optional): content of response. Defaults to None.
+            message (str, optional): additional information about error. Defaults to "".
+        """
+        self.url = url
+        self.request = request
+        self.response = response
+        message = message or self.__create_message()
+        super().__init__(message)
+
+    def get_response_error_messages(self) -> list[str]:
+        """Obtains error message from response."""
+        result = self.get_response()
+        if result is None:
+            return []
+
+        if isinstance(result, dict):
+            message = result.get("error", {}).get("message", None)
+            return [str(message)] if message is not None else []
+
+        messages = []
+        for item in result:
+            message = item.get("error", {}).get("message", None)
+            if message is not None:
+                messages.append(str(message))
+        return messages
+
+    def get_response(self) -> JsonT | list[JsonT] | None:  # noqa: D102
+        return self.response if isinstance(self.response, dict | list) else None
+
+    def _get_reply(self) -> str:
+        if (result := self.get_response()) is not None:
+            return f"{result=}"
+
+        if self.response is not None:
+            return f"response={self.response}"
+
+        return "no response available"
+
+    def __create_message(self) -> str:
+        return (
+            f"Problem occurred during communication with: url={self.url}, request={self.request}, {self._get_reply()}"
+        )
+
+
+class ExceededAmountOfRetriesError(CommunicationError):
+    """Raised if exceeded amount of retries."""
