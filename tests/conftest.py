16c5f539cb9340de5746e72d66212053d76fa17b
kmochocki <kmochocki@syncad.com>
2024-02-13 18:21:04 +0000

    Add default node

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,7 +20,10 @@ def registered_apis() -> RegisteredApisT:
 
 @pytest.fixture()
 def hived_http_endpoint(request: pytest.FixtureRequest) -> helpy.HttpUrl:
-    return helpy.HttpUrl(request.config.getoption("--hived-http-endpoint"), protocol="https")
+    raw_url = request.config.getoption("--hived-http-endpoint")
+    if raw_url is None or raw_url == "":
+        raw_url = "https://api.hive.blog"
+    return helpy.HttpUrl(raw_url)
 
 
 @pytest.fixture()
