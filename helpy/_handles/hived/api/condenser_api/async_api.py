f14f25c85f47459bc5f4520190b88db721b01a0d
kmochocki <kmochocki@syncad.com>
2024-02-15 12:31:46 +0000

    Fix positional arguments in api definitions

--- helpy/_handles/hived/api/condenser_api/async_api.py
+++ helpy/_handles/hived/api/condenser_api/async_api.py
@@ -19,19 +19,19 @@ class CondenserApi(AbstractAsyncApi, CondenserApiCommons):
         raise NotImplementedError
 
     @api
-    async def get_active_witnesses(self, /, include_future: bool = False) -> condenser_api.GetActiveWitnesses:
+    async def get_active_witnesses(self, include_future: bool = False, /) -> condenser_api.GetActiveWitnesses:
         raise NotImplementedError
 
     @api
-    async def get_block_header(self, /, block_num: int) -> condenser_api.GetBlockHeader:
+    async def get_block_header(self, block_num: int, /) -> condenser_api.GetBlockHeader:
         raise NotImplementedError
 
     @api
-    async def get_block(self, /, block_num: int) -> condenser_api.GetBlock:
+    async def get_block(self, block_num: int, /) -> condenser_api.GetBlock:
         raise NotImplementedError
 
     @api
-    async def get_ops_in_block(self, /, block_num: int, only_virtual: bool = False) -> condenser_api.GetOpsInBlock:
+    async def get_ops_in_block(self, block_num: int, only_virtual: bool = False, /) -> condenser_api.GetOpsInBlock:
         raise NotImplementedError
 
     @api
@@ -55,7 +55,7 @@ class CondenserApi(AbstractAsyncApi, CondenserApiCommons):
         raise NotImplementedError
 
     @api
-    async def get_witness_schedule(self, /, include_future: bool = False) -> condenser_api.GetWitnessSchedule:
+    async def get_witness_schedule(self, include_future: bool = False, /) -> condenser_api.GetWitnessSchedule:
         raise NotImplementedError
 
     @api
@@ -67,27 +67,27 @@ class CondenserApi(AbstractAsyncApi, CondenserApiCommons):
         raise NotImplementedError
 
     @api
-    async def get_reward_fund(self, /, name: str) -> condenser_api.GetRewardFund:
+    async def get_reward_fund(self, name: str, /) -> condenser_api.GetRewardFund:
         raise NotImplementedError
 
     @api
-    async def get_key_references(self, /, key: str) -> condenser_api.GetKeyReferences:
+    async def get_key_references(self, key: str, /) -> condenser_api.GetKeyReferences:
         raise NotImplementedError
 
     @api
     async def get_accounts(
-        self, /, accounts: list[str], delayed_votes_active: bool = True
+        self, accounts: list[str], delayed_votes_active: bool = True, /
     ) -> condenser_api.GetAccounts:
         raise NotImplementedError
 
     @api
     async def lookup_account_names(
-        self, /, accounts: list[str], delayed_votes_active: bool = True
+        self, accounts: list[str], delayed_votes_active: bool = True, /
     ) -> condenser_api.LookupAccountNames:
         raise NotImplementedError
 
     @api
-    async def lookup_accounts(self, /, lower_bound_name: str, limit: int) -> condenser_api.LookupAccounts:
+    async def lookup_accounts(self, lower_bound_name: str, limit: int, /) -> condenser_api.LookupAccounts:
         raise NotImplementedError
 
     @api
@@ -95,69 +95,69 @@ class CondenserApi(AbstractAsyncApi, CondenserApiCommons):
         raise NotImplementedError
 
     @api
-    async def get_owner_history(self, /, owner: str) -> condenser_api.GetOwnerHistory:
+    async def get_owner_history(self, owner: str, /) -> condenser_api.GetOwnerHistory:
         raise NotImplementedError
 
     @api
-    async def get_recovery_request(self, /, account: str) -> condenser_api.GetRecoveryRequest:
+    async def get_recovery_request(self, account: str, /) -> condenser_api.GetRecoveryRequest:
         raise NotImplementedError
 
     @api
     async def get_escrow(
-        self, /, start: tuple[str, int] | tuple[bool, datetime, int], limit: int, order: CondenserApi.SORT_TYPES
+        self, start: tuple[str, int] | tuple[bool, datetime, int], limit: int, order: CondenserApi.SORT_TYPES, /
     ) -> condenser_api.GetEscrow:
         raise NotImplementedError
 
     @api
     async def get_withdraw_routes(
-        self, /, account: str, destination: CondenserApi.WITHDRAW_ROUTE_TYPES
+        self, account: str, destination: CondenserApi.WITHDRAW_ROUTE_TYPES, /
     ) -> condenser_api.GetWithdrawRoutes:
         raise NotImplementedError
 
     @api
-    async def get_savings_withdraw_from(self, /, account: str) -> condenser_api.GetSavingsWithdrawFrom:
+    async def get_savings_withdraw_from(self, account: str, /) -> condenser_api.GetSavingsWithdrawFrom:
         raise NotImplementedError
 
     @api
-    async def get_savings_withdraw_to(self, /, account: str) -> condenser_api.GetSavingsWithdrawTo:
+    async def get_savings_withdraw_to(self, account: str, /) -> condenser_api.GetSavingsWithdrawTo:
         raise NotImplementedError
 
     @api
     async def get_vesting_delegations(
-        self, /, account: str, start: str, limit: int = 100
+        self, account: str, start: str, limit: int = 100, /
     ) -> condenser_api.GetVestingDelegations:
         raise NotImplementedError
 
     @api
     async def get_expiring_vesting_delegations(
-        self, /, account: str, start: str, limit: int = 100
+        self, account: str, start: str, limit: int = 100, /
     ) -> condenser_api.GetExpiringVestingDelegations:
         raise NotImplementedError
 
     @api
-    async def get_witnesses(self, /, witness_ids: list[int]) -> condenser_api.GetWitnesses:
+    async def get_witnesses(self, witness_ids: list[int], /) -> condenser_api.GetWitnesses:
         raise NotImplementedError
 
     @api
-    async def get_conversion_requests(self, /, account: str) -> condenser_api.GetConversionRequests:
+    async def get_conversion_requests(self, account: str, /) -> condenser_api.GetConversionRequests:
         raise NotImplementedError
 
     @api
     async def get_collateralized_conversion_requests(
-        self, /, account: str
+        self, account: str, /
     ) -> condenser_api.GetCollateralizedConversionRequests:
         raise NotImplementedError
 
     @api
-    async def get_witness_by_account(self, /, account: str) -> condenser_api.GetWitnessByAccount:
+    async def get_witness_by_account(self, account: str, /) -> condenser_api.GetWitnessByAccount:
         raise NotImplementedError
 
     @api
-    async def get_witnesses_by_vote(self, /, start_name: str, limit: int) -> condenser_api.GetWitnessesByVote:
+    async def get_witnesses_by_vote(self, start_name: str, limit: int, /) -> condenser_api.GetWitnessesByVote:
         raise NotImplementedError
 
     @api
-    async def lookup_witness_accounts(self, /, start: str, limit: int) -> condenser_api.LookupWitnessAccounts:
+    async def lookup_witness_accounts(self, start: str, limit: int, /) -> condenser_api.LookupWitnessAccounts:
         raise NotImplementedError
 
     @api
@@ -165,37 +165,37 @@ class CondenserApi(AbstractAsyncApi, CondenserApiCommons):
         raise NotImplementedError
 
     @api
-    async def get_open_orders(self, /, owner: str) -> condenser_api.GetOpenOrders:
+    async def get_open_orders(self, owner: str, /) -> condenser_api.GetOpenOrders:
         raise NotImplementedError
 
     @api
-    async def get_transaction_hex(self, /, transaction: TransactionLegacy) -> condenser_api.GetTransactionHex:
+    async def get_transaction_hex(self, transaction: TransactionLegacy, /) -> condenser_api.GetTransactionHex:
         raise NotImplementedError
 
     @api
-    async def get_transaction(self, /, transaction_id: str) -> condenser_api.GetTransaction:
+    async def get_transaction(self, transaction_id: str, /) -> condenser_api.GetTransaction:
         raise NotImplementedError
 
     @api
     async def get_required_signatures(
-        self, /, transaction: TransactionLegacy, public_key: str
+        self, transaction: TransactionLegacy, public_key: str, /
     ) -> condenser_api.GetRequiredSignatures:
         raise NotImplementedError
 
     @api
-    async def get_potential_signatures(self, /, transaction: TransactionLegacy) -> condenser_api.GetPotentialSignatures:
+    async def get_potential_signatures(self, transaction: TransactionLegacy, /) -> condenser_api.GetPotentialSignatures:
         raise NotImplementedError
 
     @api
-    async def verify_authority(self, /, transaction: TransactionLegacy) -> condenser_api.VerifyAuthority:
+    async def verify_authority(self, transaction: TransactionLegacy, /) -> condenser_api.VerifyAuthority:
         raise NotImplementedError
 
     @api
-    async def verify_account_authority(self, /, transaction: TransactionLegacy) -> condenser_api.VerifyAccountAuthority:
+    async def verify_account_authority(self, transaction: TransactionLegacy, /) -> condenser_api.VerifyAccountAuthority:
         raise NotImplementedError
 
     @api
-    async def get_active_votes(self, /, author: str, permlink: str) -> condenser_api.GetActiveVotes:
+    async def get_active_votes(self, author: str, permlink: str, /) -> condenser_api.GetActiveVotes:
         raise NotImplementedError
 
     @api
@@ -210,17 +210,17 @@ class CondenserApi(AbstractAsyncApi, CondenserApiCommons):
         raise NotImplementedError
 
     @api
-    async def broadcast_transaction(self, /, transaction: TransactionLegacy) -> condenser_api.BroadcastTransaction:
+    async def broadcast_transaction(self, transaction: TransactionLegacy, /) -> condenser_api.BroadcastTransaction:
         raise NotImplementedError
 
     @api
     async def broadcast_transaction_synchronous(
-        self, /, transaction: TransactionLegacy
+        self, transaction: TransactionLegacy, /
     ) -> condenser_api.BroadcastTransactionSynchronous:
         raise NotImplementedError
 
     @api
-    async def get_account_reputations(self, /, account: str, limit: int = 1000) -> condenser_api.GetAccountReputations:
+    async def get_account_reputations(self, account: str, limit: int = 1000, /) -> condenser_api.GetAccountReputations:
         raise NotImplementedError
 
     @api
@@ -232,22 +232,22 @@ class CondenserApi(AbstractAsyncApi, CondenserApiCommons):
         raise NotImplementedError
 
     @api
-    async def get_order_book(self, /, limit: int = 500) -> condenser_api.GetOrderBook:
+    async def get_order_book(self, limit: int = 500, /) -> condenser_api.GetOrderBook:
         raise NotImplementedError
 
     @api
     async def get_trade_history(
-        self, /, start: datetime, stop: datetime, limit: int = 1000
+        self, start: datetime, stop: datetime, limit: int = 1000, /
     ) -> condenser_api.GetTradeHistory:
         raise NotImplementedError
 
     @api
-    async def get_recent_trades(self, /, limit: int = 1000) -> condenser_api.GetRecentTrades:
+    async def get_recent_trades(self, limit: int = 1000, /) -> condenser_api.GetRecentTrades:
         raise NotImplementedError
 
     @api
     async def get_market_history(
-        self, /, bucket_seconds: int, start: datetime, stop: datetime
+        self, bucket_seconds: int, start: datetime, stop: datetime, /
     ) -> condenser_api.GetMarketHistory:
         raise NotImplementedError
 
@@ -256,7 +256,7 @@ class CondenserApi(AbstractAsyncApi, CondenserApiCommons):
         raise NotImplementedError
 
     @api
-    async def is_known_transaction(self, /, transaction_id: str) -> condenser_api.IsKnownTransaction:
+    async def is_known_transaction(self, transaction_id: str, /) -> condenser_api.IsKnownTransaction:
         raise NotImplementedError
 
     @api
@@ -272,7 +272,7 @@ class CondenserApi(AbstractAsyncApi, CondenserApiCommons):
         raise NotImplementedError
 
     @api
-    async def find_proposals(self, /, proposals_ids: list[int]) -> condenser_api.FindProposals:
+    async def find_proposals(self, proposals_ids: list[int], /) -> condenser_api.FindProposals:
         raise NotImplementedError
 
     @api
@@ -287,19 +287,19 @@ class CondenserApi(AbstractAsyncApi, CondenserApiCommons):
         raise NotImplementedError
 
     @api
-    async def find_recurrent_transfers(self, /, account: str) -> condenser_api.FindRecurrentTransfers:
+    async def find_recurrent_transfers(self, account: str, /) -> condenser_api.FindRecurrentTransfers:
         raise NotImplementedError
 
     @api
-    async def find_rc_accounts(self, /, accounts: list[str]) -> condenser_api.FindRcAccounts:
+    async def find_rc_accounts(self, accounts: list[str], /) -> condenser_api.FindRcAccounts:
         raise NotImplementedError
 
     @api
-    async def list_rc_accounts(self, /, start: str, limit: int) -> condenser_api.ListRcAccounts:
+    async def list_rc_accounts(self, start: str, limit: int, /) -> condenser_api.ListRcAccounts:
         raise NotImplementedError
 
     @api
     async def list_rc_direct_delegations(
-        self, /, start: tuple[str, str], limit: int
+        self, start: tuple[str, str], limit: int, /
     ) -> condenser_api.ListRcDirectDelegations:
         raise NotImplementedError
