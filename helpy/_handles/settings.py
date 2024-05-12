7988cf6a7af2fee97cdeaf178475d03dc82dcc1f
kmochocki <kmochocki@syncad.com>
2024-05-12 22:36:58 +0000

    Move obtaining instance of communicator to settings

--- helpy/_handles/settings.py
+++ helpy/_handles/settings.py
@@ -5,12 +5,22 @@ from pathlib import Path
 from pydantic import Field
 
 from helpy._communication.abc.communicator import AbstractCommunicator  # noqa: TCH001
-from helpy._communication.httpx_communicator import HttpxCommunicator
 from helpy._communication.settings import CommunicationSettings
 from helpy._interfaces.url import HttpUrl  # noqa: TCH001
 
 
 class HandleSettings(CommunicationSettings):
     http_endpoint: HttpUrl
-    communicator: type[AbstractCommunicator] | AbstractCommunicator = HttpxCommunicator
+    communicator: type[AbstractCommunicator] | AbstractCommunicator | None = None
     working_directory: Path = Field(default_factory=lambda: Path("."))
+
+    def try_get_communicator_instance(
+        self, settings: CommunicationSettings | None = None
+    ) -> AbstractCommunicator | None:
+        if self.communicator is None:
+            return None
+
+        if isinstance(self.communicator, type):
+            return self.communicator(settings=(settings or self))
+
+        return self.communicator
