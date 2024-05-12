1136500c419dfbdce03bde2864715d0ffe413514
kmochocki <kmochocki@syncad.com>
2024-05-12 22:53:31 +0000

    Move KeyPair from schemas

--- helpy/__init__.py
+++ helpy/__init__.py
@@ -13,12 +13,14 @@ from helpy._interfaces import wax
 from helpy._interfaces.asset import Hf26Asset, LegacyAsset
 from helpy._interfaces.config import Config as AbstractConfig
 from helpy._interfaces.context import ContextAsync, ContextSync
+from helpy._interfaces.key_pair import KeyPair
 from helpy._interfaces.stopwatch import Stopwatch
 from helpy._interfaces.time import OffsetTimeControl, SpeedUpRateTimeControl, StartTimeControl, Time, TimeFormats
 from helpy._interfaces.transaction_helper import Transaction
 from helpy._interfaces.url import HttpUrl, P2PUrl, WsUrl
 
 __version__ = "0.0.0"
+
 
 __all__ = [
     "AbstractConfig",
@@ -32,6 +34,7 @@ __all__ = [
     "Hived",
     "HivedNotificationHandler",
     "HttpUrl",
+    "KeyPair",
     "LegacyAsset",
     "OffsetTimeControl",
     "P2PUrl",
