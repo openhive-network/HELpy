0b72ee047fe32572e86f7084d356f8bdbd719c21
kmochocki <kmochocki@syncad.com>
2024-05-12 22:36:33 +0000

    Add AioHttpCommunicator

--- helpy/communicators.py
+++ helpy/communicators.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
 from helpy._communication.abc.communicator import AbstractCommunicator
+from helpy._communication.aiohttp_communicator import AioHttpCommunicator
 from helpy._communication.httpx_communicator import HttpxCommunicator
 from helpy._communication.request_communicator import RequestCommunicator
 from helpy._communication.universal_notification_server import UniversalNotificationHandler, UniversalNotificationServer
 
 __all__ = [
     "AbstractCommunicator",
+    "AioHttpCommunicator",
     "UniversalNotificationHandler",
     "UniversalNotificationServer",
     "RequestCommunicator",
