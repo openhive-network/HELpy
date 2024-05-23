aaef9e29955be262a0449843fb19be40d63854e8
kmochocki <kmochocki@syncad.com>
2024-05-23 12:38:34 +0000

    Use (A)Sync versions of session holder in beekeepy handles

--- helpy/_handles/beekeeper/handle.py
+++ helpy/_handles/beekeeper/handle.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import uuid
-from typing import TYPE_CHECKING, Any, cast
+from typing import TYPE_CHECKING, Any, NoReturn, cast
 
 from helpy._handles.abc.handle import AbstractAsyncHandle, AbstractSyncHandle
 from helpy._handles.batch_handle import ApiFactory, AsyncBatchHandle, SyncBatchHandle
@@ -9,13 +9,13 @@ from helpy._handles.beekeeper.api.api_collection import (
     BeekeeperAsyncApiCollection,
     BeekeeperSyncApiCollection,
 )
-from helpy._handles.beekeeper.api.session_holder import SessionHolder
-from helpy._handles.settings import HandleSettings
+from helpy._handles.beekeeper.api.session_holder import AsyncSessionHolder, SyncSessionHolder
 
 if TYPE_CHECKING:
     from typing_extensions import Self
 
     from helpy._communication.abc.communicator import AbstractCommunicator
+    from helpy._handles.beekeeper.api import AsyncBeekeeperApi, SyncBeekeeperApi
     from helpy._interfaces.url import HttpUrl
 
 _handle_target_service_name = "beekeeper"
@@ -25,11 +25,11 @@ def _random_string() -> str:
     return str(uuid.uuid4())
 
 
-def _disabled_api() -> str:
-    return ""
+def _raise_acquire_not_possible() -> NoReturn:
+    raise RuntimeError("Batch handle has predefined token and should not create its own")
 
 
-class _SyncSessionBatchHandle(SyncBatchHandle[BeekeeperSyncApiCollection], SessionHolder):
+class _SyncSessionBatchHandle(SyncBatchHandle[BeekeeperSyncApiCollection], SyncSessionHolder):
     def __init__(
         self,
         url: HttpUrl,
@@ -44,10 +44,10 @@ class _SyncSessionBatchHandle(SyncBatchHandle[BeekeeperSyncApiCollection], Sessi
         self._set_session_token(session_token)
 
     def _acquire_session_token(self) -> str:
-        raise NotImplementedError
+        _raise_acquire_not_possible()
 
 
-class _AsyncSessionBatchHandle(AsyncBatchHandle[BeekeeperAsyncApiCollection], SessionHolder):
+class _AsyncSessionBatchHandle(AsyncBatchHandle[BeekeeperAsyncApiCollection], AsyncSessionHolder):
     def __init__(
         self,
         url: HttpUrl,
@@ -61,22 +61,19 @@ class _AsyncSessionBatchHandle(AsyncBatchHandle[BeekeeperAsyncApiCollection], Se
         super().__init__(url, communicator, api, *args, delay_error_on_data_access=delay_error_on_data_access, **kwargs)
         self._set_session_token(session_token)
 
-    def _acquire_session_token(self) -> str:
-        raise NotImplementedError
+    async def _acquire_session_token(self) -> str:
+        _raise_acquire_not_possible()
 
 
-class Beekeeper(AbstractSyncHandle, SessionHolder):
+class Beekeeper(AbstractSyncHandle, SyncSessionHolder):
     """Synchronous handle for beekeeper service communication."""
-
-    def _clone(self) -> Beekeeper:
-        return Beekeeper(http_url=self.http_endpoint, settings=self.settings)
 
     def _construct_api(self) -> BeekeeperSyncApiCollection:
         return BeekeeperSyncApiCollection(owner=self)
 
     @property
-    def api(self) -> BeekeeperSyncApiCollection:
-        return cast(BeekeeperSyncApiCollection, super().api)
+    def api(self) -> SyncBeekeeperApi:  # type: ignore[override]
+        return cast(BeekeeperSyncApiCollection, super().api).beekeeper
 
     def _target_service(self) -> str:
         return _handle_target_service_name
@@ -91,54 +88,36 @@ class Beekeeper(AbstractSyncHandle, SessionHolder):
         )
 
     def _acquire_session_token(self) -> str:
-        return self.api.beekeeper.create_session(
-            notifications_endpoint=self._get_notification_endpoint(), salt=self._get_salt()
-        ).token
-
-    def _get_notification_endpoint(self) -> str:
-        return _disabled_api()
+        return self.api.create_session(salt=self._get_salt()).token
 
     def _get_salt(self) -> str:
         return _random_string()
 
 
-class AsyncBeekeeper(AbstractAsyncHandle, SessionHolder):
+class AsyncBeekeeper(AbstractAsyncHandle, AsyncSessionHolder):
     """Asynchronous handle for beekeeper service communication."""
-
-    def _clone(self) -> AsyncBeekeeper:
-        return AsyncBeekeeper(http_url=self.http_endpoint, settings=self.settings)
 
     def _construct_api(self) -> BeekeeperAsyncApiCollection:
         return BeekeeperAsyncApiCollection(owner=self)
 
     @property
-    def api(self) -> BeekeeperAsyncApiCollection:
-        return cast(BeekeeperAsyncApiCollection, super().api)
+    def api(self) -> AsyncBeekeeperApi:  # type: ignore[override]
+        return cast(BeekeeperAsyncApiCollection, super().api).beekeeper
 
     def _target_service(self) -> str:
         return _handle_target_service_name
 
-    def batch(self, *, delay_error_on_data_access: bool = False) -> AsyncBatchHandle[BeekeeperAsyncApiCollection]:
+    async def batch(self, *, delay_error_on_data_access: bool = False) -> AsyncBatchHandle[BeekeeperAsyncApiCollection]:
         return _AsyncSessionBatchHandle(
             url=self.http_endpoint,
             communicator=self._communicator,
             api=lambda o: BeekeeperAsyncApiCollection(owner=o),
             delay_error_on_data_access=delay_error_on_data_access,
-            session_token=self.session_token,
+            session_token=await self.session_token,
         )
 
-    def _acquire_session_token(self) -> str:
-        # FIXME: how to make it work both in async and sync mode
-        return (
-            Beekeeper(settings=HandleSettings(http_endpoint=self.http_endpoint))
-            .api.beekeeper.create_session(
-                notifications_endpoint=self._get_notification_endpoint(), salt=self._get_salt()
-            )
-            .token
-        )
-
-    def _get_notification_endpoint(self) -> str:
-        return _disabled_api()
+    async def _acquire_session_token(self) -> str:
+        return (await self.api.create_session(salt=self._get_salt())).token
 
     def _get_salt(self) -> str:
         return _random_string()
