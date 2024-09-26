8b9bf2bec6f31a158e9337770006c60b84ac9d4e
kmochocki <kmochocki@syncad.com>
2024-09-26 10:59:34 +0000

    Add explicit conversion to bytes in communicators
    This is to avoid custom encoding by certain libraries with non utf-8 encoding

--- helpy/helpy/_communication/abc/communicator.py
+++ helpy/helpy/_communication/abc/communicator.py
@@ -17,11 +17,11 @@ class AbstractCommunicator(SharedSettingsHolder[CommunicationSettings], ABC):
     """Provides basic interface for communicators, which can implement communications using different way."""
 
     @abstractmethod
-    def send(self, url: HttpUrl, data: str) -> str:
+    def _send(self, url: HttpUrl, data: bytes) -> str:
         """Sends to given url given data synchronously."""
 
     @abstractmethod
-    async def async_send(self, url: HttpUrl, data: str) -> str:
+    async def _async_send(self, url: HttpUrl, data: bytes) -> str:
         """Sends to given url given data asynchronously."""
 
     async def _async_sleep_for_retry(self) -> None:
@@ -37,15 +37,25 @@ class AbstractCommunicator(SharedSettingsHolder[CommunicationSettings], ABC):
         return amount > self.settings.max_retries
 
     @classmethod
+    def _encode_data(cls, data: str) -> bytes:
+        return data.encode("utf-8")
+
+    @classmethod
     def _json_headers(cls) -> dict[str, str]:
         """Returns headers for json communication."""
         return {"Content-Type": "application/json"}
 
     @classmethod
-    def _assert_status_code(cls, *, status_code: int, sent: str, received: str) -> None:
+    def _assert_status_code(cls, *, status_code: int, sent: str | bytes, received: str) -> None:
         """Checks is received status code is 2xx."""
         ok_status_code_lower_bound = 200
         ok_status_code_upper_bound = 299
 
         if not (ok_status_code_lower_bound <= status_code <= ok_status_code_upper_bound):
             raise CommunicationError(f"{status_code=}", f"{sent=}", f"{received=}")
+
+    def send(self, url: HttpUrl, data: str) -> str:
+        return self._send(url, self._encode_data(data))
+
+    async def async_send(self, url: HttpUrl, data: str) -> str:
+        return await self._async_send(url, self._encode_data(data))
