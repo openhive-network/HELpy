1849a63a116ae7122118f58ea1b7f1b21c5aae11
kmochocki <kmochocki@syncad.com>
2024-09-27 11:01:25 +0000

    Add default timeout class var to communication settings

--- helpy/helpy/_communication/settings.py
+++ helpy/helpy/_communication/settings.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from datetime import timedelta
 from os import environ
-from typing import TYPE_CHECKING, Any, cast
+from typing import TYPE_CHECKING, Any, ClassVar, cast
 
 from pydantic import BaseModel, Field
 
@@ -20,10 +20,15 @@ def _default_factory(env_name: str, default_factory: Callable[[str | None], Any]
 
 
 class CommunicationSettings(BaseModel):
+    DEFAULT_TIMEOUT: ClassVar[timedelta] = timedelta(seconds=5)
+
     max_retries: int = _default_factory("HELPY_COMMUNICATION_MAX_RETRIES", lambda x: int(x or 5))
     """Amount of retries when sending request to service."""
 
-    timeout: timedelta = _default_factory("HELPY_COMMUNICATION_TIMEOUT_SECS", lambda x: timedelta(seconds=int(x or 5)))
+    timeout: timedelta = _default_factory(
+        "HELPY_COMMUNICATION_TIMEOUT_SECS",
+        lambda x: (CommunicationSettings.DEFAULT_TIMEOUT if x is None else timedelta(seconds=int(x))),
+    )
     """Maximum time for request to finish."""
 
     period_between_retries: timedelta = _default_factory(
