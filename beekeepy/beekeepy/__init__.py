c0d6a4fd10184f41a3c9589a0533b42528451a8a
kmochocki <kmochocki@syncad.com>
2025-01-08 16:24:26 +0000

    Expose missing classes in library interface

--- beekeepy/beekeepy/__init__.py
+++ beekeepy/beekeepy/__init__.py
@@ -3,17 +3,19 @@ from __future__ import annotations
 from beekeepy._handle import close_already_running_beekeeper
 from beekeepy._interface.abc.asynchronous.beekeeper import Beekeeper as AsyncBeekeeper
 from beekeepy._interface.abc.asynchronous.session import Session as AsyncSession
+from beekeepy._interface.abc.asynchronous.wallet import UnlockedWallet as AsyncUnlockedWallet
 from beekeepy._interface.abc.asynchronous.wallet import Wallet as AsyncWallet
 from beekeepy._interface.abc.packed_object import PackedAsyncBeekeeper, PackedSyncBeekeeper
 from beekeepy._interface.abc.synchronous.beekeeper import Beekeeper
 from beekeepy._interface.abc.synchronous.session import Session
-from beekeepy._interface.abc.synchronous.wallet import Wallet
+from beekeepy._interface.abc.synchronous.wallet import UnlockedWallet, Wallet
 from beekeepy._interface.settings import Settings
 
 __all__ = [
     "AsyncBeekeeper",
     "AsyncSession",
     "AsyncWallet",
+    "AsyncUnlockedWallet",
     "Beekeeper",
     "close_already_running_beekeeper",
     "PackedAsyncBeekeeper",
@@ -21,4 +23,5 @@ __all__ = [
     "Session",
     "Settings",
     "Wallet",
+    "UnlockedWallet",
 ]
