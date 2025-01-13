7a7882703def70b94c1d3ba94a51d5d5211c03e6
Jakub Ziebinski <ziebinskijakub@gmail.com>
2025-01-13 08:45:55 +0100

    Move UnlockedWallet and AsyncUnlockedWallet to the main __init__.py

--- beekeepy/beekeepy/__init__.py
+++ beekeepy/beekeepy/__init__.py
@@ -3,11 +3,12 @@ from __future__ import annotations
 from beekeepy._handle import close_already_running_beekeeper
 from beekeepy._interface.abc.asynchronous.beekeeper import Beekeeper as AsyncBeekeeper
 from beekeepy._interface.abc.asynchronous.session import Session as AsyncSession
+from beekeepy._interface.abc.asynchronous.wallet import UnlockedWallet as AsyncUnlockedWallet
 from beekeepy._interface.abc.asynchronous.wallet import Wallet as AsyncWallet
 from beekeepy._interface.abc.packed_object import PackedAsyncBeekeeper, PackedSyncBeekeeper
 from beekeepy._interface.abc.synchronous.beekeeper import Beekeeper
 from beekeepy._interface.abc.synchronous.session import Session
-from beekeepy._interface.abc.synchronous.wallet import Wallet
+from beekeepy._interface.abc.synchronous.wallet import UnlockedWallet, Wallet
 from beekeepy._interface.settings import Settings
 
 __all__ = [
@@ -21,4 +22,6 @@ __all__ = [
     "Session",
     "Settings",
     "Wallet",
+    "UnlockedWallet",
+    "AsyncUnlockedWallet",
 ]
