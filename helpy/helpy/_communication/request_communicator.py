0795de452450147dd6e291e55ddede8aa08db779
Michał Kudela <kudmich@syncad.com>
2025-01-16 10:57:37 +0100

    Handle ReadTimeout error in RequestCommunicator class

--- helpy/helpy/_communication/request_communicator.py
+++ helpy/helpy/_communication/request_communicator.py
@@ -34,6 +34,8 @@ class RequestCommunicator(AbstractCommunicator):
                 data_received = response.content.decode()
                 self._assert_status_code(status_code=response.status_code, sent=data, received=data_received)
                 return data_received  # noqa: TRY300
+            except requests.exceptions.ReadTimeout as error:
+                last_exception = error
             except requests.exceptions.ConnectionError as error:
                 raise CommunicationError(url=url.as_string(), request=data) from error
             except requests.exceptions.RequestException as error:
