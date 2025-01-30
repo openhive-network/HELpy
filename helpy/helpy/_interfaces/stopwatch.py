030cfc1cc7560f4100044524a2c5ec45d5ee7303
kmochocki <kmochocki@syncad.com>
2025-01-30 12:06:35 +0000

    Extend StopwarchResult class with lap property

--- helpy/helpy/_interfaces/stopwatch.py
+++ helpy/helpy/_interfaces/stopwatch.py
@@ -20,17 +20,22 @@ class StopwatchResult:
     def delta(self) -> timedelta:
         return self.stopped_at - self.started_at
 
+    @property
+    def lap(self) -> float:
+        return (Stopwatch.now() - self.started_at).total_seconds()
+
 
 class Stopwatch(ContextSync[StopwatchResult]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.__result = StopwatchResult(started_at=self.__now(), stopped_at=self.__now())
+        self.__result = StopwatchResult(started_at=self.now(), stopped_at=self.now())
         super().__init__(*args, **kwargs)
 
     def _enter(self) -> StopwatchResult:
         return self.__result
 
     def _finally(self) -> None:
-        self.__result.stopped_at = self.__now()
+        self.__result.stopped_at = self.now()
 
-    def __now(self) -> datetime:
+    @classmethod
+    def now(cls) -> datetime:
         return datetime.now(tz=timezone.utc)
