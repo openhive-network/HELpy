efabc4123a47286767551b5ec3068e734800e6f4
kmochocki <kmochocki@syncad.com>
2025-02-05 03:20:27 +0000

    Url adds automatically missing proto + tests

--- tests/helpy_test/unit/url_tests/test_url_parsing.py
+++ tests/helpy_test/unit/url_tests/test_url_parsing.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
+import re
+from typing import Callable
+
 import pytest
 
-from helpy import HttpUrl, WsUrl
+from helpy import HttpUrl, P2PUrl, WsUrl
 from tests.helpy_test.unit.constants import DEFAULT_ADDRESS, DEFAULT_PORT, URL_TYPES
 
 
@@ -57,3 +60,33 @@ def test_url_parsing_without_address_given(url_type: URL_TYPES) -> None:
         url_type(f":{DEFAULT_PORT}")
 
     assert str(exception.value) == "Address was not specified."
+
+
+@pytest.mark.parametrize("url_cls", [HttpUrl, WsUrl, P2PUrl])
+@pytest.mark.parametrize("schema_input", [lambda x: x._allowed_protocols()[0], lambda _: None])
+def test_schema_auto_apply_in_url(
+    schema_input: Callable[[URL_TYPES | type[P2PUrl]], str | None], url_cls: URL_TYPES | type[P2PUrl]
+) -> None:
+    # ARRANGE
+    address = "some-address"
+    default_schema = url_cls._allowed_protocols()[0]
+    if default_schema:
+        default_schema += "://"
+
+    # ACT
+    url = url_cls(address, protocol=schema_input(url_cls))  # type: ignore[arg-type]
+
+    # ASSERT
+    assert url.as_string() == f"{default_schema}{address}"
+
+
+@pytest.mark.parametrize("url_cls", [HttpUrl, WsUrl, P2PUrl])
+def test_url_schema_validation(url_cls: URL_TYPES | type[P2PUrl]) -> None:
+    # ARRANGE
+    invalid_proto = "baaaad"
+
+    # ACT & ASSERT
+    with pytest.raises(
+        ValueError, match=re.escape(f"Unknown protocol: `{invalid_proto}`, allowed: {url_cls._allowed_protocols()}")
+    ):
+        url_cls("some-address", protocol=invalid_proto)  # type: ignore[arg-type]
