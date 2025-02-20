1f86655b319d166bc1d2e888e8f03ceb488378da
kmochocki <kmochocki@syncad.com>
2025-02-20 10:22:08 +0000

    Move test from clive

--- tests/beekeepy_test/handle/commandline/application_options/test_default_values.py
+++ tests/beekeepy_test/handle/commandline/application_options/test_default_values.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import pytest
+
 from beekeepy._executable.beekeeper_config import (
     BeekeeperConfig,
     http_webserver_default,
@@ -30,3 +32,57 @@ def test_default_values(beekeeper_exe: BeekeeperExecutable) -> None:
 
     # ASSERT
     check_default_values_from_config(default_config)
+
+
+async def test_config_fields_coverege(beekeeper_exe: BeekeeperExecutable) -> None:
+    """Test will check for the differences between config.ini file and BeekeeperConfig class."""
+    # ARRANGE
+    beekeeper_config = beekeeper_exe.generate_default_config()
+
+    with (beekeeper_exe.working_directory / (BeekeeperConfig.DEFAULT_FILE_NAME + ".tmp")).open() as config:
+        config_class_fields = set(beekeeper_config.__dict__.keys())
+        in_config_fields = set()
+
+        comment_line_start = "# "
+        separator_mark = "="
+
+        # ACT
+        for line in config:
+            if (line := line.strip("\n")) and separator_mark in line:
+                field_name = (
+                    line[2:].split(separator_mark)[0]
+                    if line.startswith(comment_line_start)
+                    else line.split(separator_mark)[0]
+                )
+                in_config_fields.add(field_name.replace("-", "_"))
+
+        differences_in_config = in_config_fields - config_class_fields
+        differences_in_class = config_class_fields - in_config_fields
+
+        # ASSERT
+        for diff_in_config in differences_in_config:
+            pytest.fail(
+                f"Field `{diff_in_config}` exists in config.ini file, but does not have representation in"
+                " BeekeeperConfig class."
+            )
+
+        for diff_in_class in differences_in_class:
+            if diff_in_class in [
+                "notifications_endpoint",
+                "export_keys_wallet",
+                "log_json_rpc",
+                "webserver_unix_endpoint",
+                "webserver_ws_endpoint",
+            ]:
+                # notifications_endpoint - When running not via handle, this is not set
+                # export_keys_wallet - set only when dumping keys, not this case
+                # log_json_rpc - using default logging config
+
+                # Not supported or not used
+                # webserver_unix_endpoint
+                # webserver_ws_endpoint
+                continue
+            pytest.fail(
+                f"Field `{diff_in_class}` exists in BeekeeperConfig class, but does not have representation in"
+                " config.ini file."
+            )
