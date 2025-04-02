a171daabf47bf84cf0685f2f2c2e754411fdd9e4
Radek Masłowski <rmaslowski@syncad.com>
2025-04-02 07:24:08 +0200

    wip

--- tests/beekeepy_test/handle/basic/test_keys.py
+++ tests/beekeepy_test/handle/basic/test_keys.py
@@ -22,18 +22,18 @@ PRIVATE_AND_PUBLIC_KEYS: Final[list[tuple[str, str]]] = [
         "5J7m49WCKnRBTo1HyJisBinn8Lk3syYaXsrzdFmfDxkejHLwZ1m",
         "STM5hjCkhcMKcXQMppa97XUbDR5dWC3c2K8h23P1ajEi2fT9YuagW",
     ),
-    # (
-    #     "5JqStPwQgnXBdyRDDxCDyjfC8oNjgJuZsBkT6MMz6FopydAebbC",
-    #     "STM5EjyFcCNidBSivAtTKvWWwWRNjakcRMB79QrbwMKprcTRBHtXz",
-    # ),
-    # (
-    #     "5JowdvuiDxoeLhzoSEKK74TCiwTaUHvxtRH3fkbweVEJZEsQJoc",
-    #     "STM8RgQ3yexUZjcVGxQ2i3cKywwKwhxqzwtCHPQznGUYvQ15ZvahW",
-    # ),
-    # (
-    #     "5Khrc9PX4S8wAUUmX4h2JpBgf4bhvPyFT5RQ6tGfVpKEudwpYjZ",
-    #     "STM77P1n96ojdXcKpd5BRUhVmk7qFTfM2q2UkWSKg63Xi7NKyK2Q1",
-    # ),
+    (
+        "5JqStPwQgnXBdyRDDxCDyjfC8oNjgJuZsBkT6MMz6FopydAebbC",
+        "STM5EjyFcCNidBSivAtTKvWWwWRNjakcRMB79QrbwMKprcTRBHtXz",
+    ),
+    (
+        "5JowdvuiDxoeLhzoSEKK74TCiwTaUHvxtRH3fkbweVEJZEsQJoc",
+        "STM8RgQ3yexUZjcVGxQ2i3cKywwKwhxqzwtCHPQznGUYvQ15ZvahW",
+    ),
+    (
+        "5Khrc9PX4S8wAUUmX4h2JpBgf4bhvPyFT5RQ6tGfVpKEudwpYjZ",
+        "STM77P1n96ojdXcKpd5BRUhVmk7qFTfM2q2UkWSKg63Xi7NKyK2Q1",
+    ),
 ]
 
 AllowedListOfPublicKeysT = list[PublicKey] | list[PublicKeyItem] | list[ImportKey] | list[str]
@@ -65,32 +65,32 @@ def test_key_import(beekeeper: Beekeeper, prv_pub: tuple[str, str], wallet: Wall
     assert_keys(beekeeper.api.get_public_keys().keys, [prv_pub[1]])
 
 
-# def test_import_multiple_keys(beekeeper: Beekeeper, wallet: WalletInfo) -> None:
-#     # ARRANGE & ACT
-#     import_keys_result: list[ImportKey] = [
-#         beekeeper.api.import_key(wallet_name=wallet.name, wif_key=prv) for prv, _ in PRIVATE_AND_PUBLIC_KEYS
-#     ]
-#     public_keys = [x.public_key for x in import_keys_result]
-#     received_public_keys = beekeeper.api.get_public_keys().keys
+def test_import_multiple_keys(beekeeper: Beekeeper, wallet: WalletInfo) -> None:
+    # ARRANGE & ACT
+    import_keys_result: list[ImportKey] = [
+        beekeeper.api.import_key(wallet_name=wallet.name, wif_key=prv) for prv, _ in PRIVATE_AND_PUBLIC_KEYS
+    ]
+    public_keys = [x.public_key for x in import_keys_result]
+    received_public_keys = beekeeper.api.get_public_keys().keys
 
-#     # ASSERT
-#     assert_keys(received_public_keys, public_keys)
+    # ASSERT
+    assert_keys(received_public_keys, public_keys)
 
 
-# @pytest.mark.parametrize("key_pair", PRIVATE_AND_PUBLIC_KEYS)
-# def test_remove_key(
-#     beekeeper: Beekeeper,
-#     wallet: WalletInfo,
-#     key_pair: tuple[PublicKey, PrivateKey],
-# ) -> None:
-#     # ARRANGE
-#     private_key, public_key = [PrivateKey(key_pair[0]), PublicKey(key_pair[1])]
-#     beekeeper.api.import_key(wallet_name=wallet.name, wif_key=private_key)
+@pytest.mark.parametrize("key_pair", PRIVATE_AND_PUBLIC_KEYS)
+def test_remove_key(
+    beekeeper: Beekeeper,
+    wallet: WalletInfo,
+    key_pair: tuple[PublicKey, PrivateKey],
+) -> None:
+    # ARRANGE
+    private_key, public_key = [PrivateKey(key_pair[0]), PublicKey(key_pair[1])]
+    beekeeper.api.import_key(wallet_name=wallet.name, wif_key=private_key)
 
-#     # ACT & ASSERT
-#     assert_keys((beekeeper.api.get_public_keys()).keys, [public_key])
-#     beekeeper.api.remove_key(
-#         wallet_name=wallet.name,
-#         public_key=public_key,
-#     )
-#     assert_keys((beekeeper.api.get_public_keys()).keys)
+    # ACT & ASSERT
+    assert_keys((beekeeper.api.get_public_keys()).keys, [public_key])
+    beekeeper.api.remove_key(
+        wallet_name=wallet.name,
+        public_key=public_key,
+    )
+    assert_keys((beekeeper.api.get_public_keys()).keys)
